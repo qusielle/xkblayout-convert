14ffe66ebd8644174b80985bc423c54d4b7b9482
Qusielle <qusielle@gmail.com>
2021-01-02 13:12:31 +0100

    Split logic to separate functions
    Problem:
    Current structure is function-less and linear, which prevents from
    developing new functionality in a proper way.
    
    Solution:
    Split the code to functions for converting the selected text and for
    waiting for keys being released.
    As a side-effect of change, `sleep` is done before a text copying.

--- xkblayout-convert.sh
+++ xkblayout-convert.sh
@@ -4,25 +4,52 @@ set -e
 en_layout='qwertyuiop[]\\asdfghjkl;'\''zxcvbnm,.\/~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?~`'
 ru_layout='йцукенгшщзхъ\\фывапролджэячсмитьбю.Ё!"№;%:?*()_+ЙЦУКЕНГШЩЗХЪ\/ФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ёё'
 
-xdotool key ctrl+Insert
-clipboard_text=$(xclip -o)
 
-current_layout=$(xkblayout-state print '%n')
+# Wait until keyboard keys pressed by user are released.
+#
+# This is needed since the typing of target text by `xdotool type` is affected
+# by pressed modificators, so let user release the keys were used to trigger
+# this script. For example, if `Shift` button is pressed, then target lowercase
+# text will be typed in uppercase and vice versa.
+#
+# Note: `--clearmodifiers` argument to `xdotool` often inverts the modificator
+# key status in the end depending on when the key was released, so it was
+# decided not to use that.
+wait_for_keys_released() {
+    # TODO: Find the proper way to detect that keys are released. All found
+    #       solutions require superuser rights, which is not applicable here.
+    sleep 0.5
+}
 
-# Since `xdotool type` is affected by pressed key modificators (e.g. Shift),
-# do sleep for a while to let user release the keys were used to trigger this
-# script.
-# `--clearmodifiers` argument to `xdotool` often inverts the modificator key
-# status in the end depending on when the key was released, so nothing better
-# than stable `sleep`.
-sleep 0.5
 
-if [[ $current_layout == English ]]; then
-    translated_text=$(sed -e "y/$en_layout/$ru_layout/" <<<"$clipboard_text")
-    xdotool type "$translated_text"
-    xkb-switch -s ru
-elif [[ $current_layout == Russian ]]; then
-    translated_text=$(sed -e "y/$ru_layout/$en_layout/" <<<"$clipboard_text")
-    xdotool type "$translated_text"
-    xkb-switch -s us
-fi
+# Convert text that is already selected.
+#
+# Proceed in the following steps:
+# * Copy text to clipboard by pressing `ctrl+Insert`.
+# * Detect current layout.
+# * Translate characters from current layout to opposite.
+# * Type converted text.
+# * Switch keyboard layout to opposite.
+convert_selected_text() {
+    xdotool key ctrl+Insert
+    local -r clipboard_text=$(xclip -o)
+    local -r current_layout=$(xkblayout-state print '%n')
+
+    local translated_text
+    if [[ $current_layout == English ]]; then
+        translated_text=$(sed -e "y/$en_layout/$ru_layout/" <<<"$clipboard_text")
+        xdotool type "$translated_text"
+        xkb-switch -s ru
+    elif [[ $current_layout == Russian ]]; then
+        translated_text=$(sed -e "y/$ru_layout/$en_layout/" <<<"$clipboard_text")
+        xdotool type "$translated_text"
+        xkb-switch -s us
+    fi
+}
+
+main() {
+    wait_for_keys_released
+    convert_selected_text
+}
+
+main
